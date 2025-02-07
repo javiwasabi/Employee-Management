d0ef603a19d6fb63fc70768dd4788159ebc0061e
javiwasabi <javiera.osoriom@usm.cl>
2025-02-07 15:18:07 -0300

    aded gallant in allowedOrigins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://employee-management-brown-five.vercel.app'
+    'https://employee-management-brown-five.vercel.app',
+    'gallant-stillness-production.up.railway.app'
   
 ]
 
