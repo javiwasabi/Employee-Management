a9e0c08e9289e86e39b1c04bfb9046e1652cecc9
javiwasabi <javiera.osoriom@usm.cl>
2025-03-17 10:35:33 -0300

    added PATCH to cors

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -5,7 +5,7 @@ const corsOptions = {
         "https://employee-management-brown-five.vercel.app",
         "https://gallant-stillness-production.up.railway.app"
     ],
-    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // 🔹 Importante si usas cookies o autenticación
     optionsSuccessStatus: 200
